241486cf586e0654bd700f739f425bcf4aa34b66
Jasim Uddin <jasimbdpro@gmail.com>
2024-07-05 16:21:46 +0600

    nw

--- src/components/SecretVideo.jsx
+++ src/components/SecretVideo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SecretVideo = () => {
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/9V7Fps960Co?si=DN24yoTmrZ9NHbPY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Ydiz1Hzfx5s?si=mjealrhYqBlBdcN3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     );
 };
